ce83fdd234ca6fd6af01aab2df2c45865d2ad52b
Maksim <ploma2020@mail.ru>
2025-05-24 11:49:38 +0300

    Add pop-up

---
+++ src/pop-up-window.js
@@ -0,0 +1,48 @@
+const key = 'key';
+const showDelay = 10000;
+const storageTime = 30000;
+
+function shouldShowPopup() {
+  const lastShown = localStorage.getItem(key);
+  if (!lastShown) return true;
+
+  const lastDate = new Date(lastShown);
+  const now = new Date();
+
+  const diff = now - lastDate;
+
+  return diff >= storageTime;
+}
+
+function showPopup() {
+  document.body.style.overflow = 'hidden';
+
+  const formTemplate = document.querySelector('#pop-up-window').content;
+  const form = formTemplate.querySelector('.pop-up-window').cloneNode(true);
+  form.classList.add('opened-card');
+
+  const overlay = document.createElement('div');
+  overlay.classList.add('overlay');
+
+  overlay.append(form);
+  document.body.append(overlay);
+
+  overlay.addEventListener('click', (evt) => {
+    if (evt.target === overlay) {
+      closeForm(overlay);
+    }
+  });
+
+  localStorage.setItem(key, String(new Date()));
+}
+
+function closePopup() {
+  overlay.remove();
+  document.body.style.overflow = null;
+}
+
+window.addEventListener('load', () => {
+  if (shouldShowPopup()) {
+    setTimeout(showPopup, showDelay);
+  }
+});
